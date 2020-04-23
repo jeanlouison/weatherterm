71a0fd02b70ea3df5f38490f237a8e9addca7eaa
lallemann <pro.louis.allemann@gmail.com>
2020-04-23 16:38:26 +0200

    ignored venv

--- src/weatherterm/core/forecast_type.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum, unique
-
-@unique
-class ForecastType(Enum):
-    TODAY = 'today'
-    FIVEDAYS = '5day'
-    TENDAYS = '10day'
-    WEEKEND = 'weekend'
--- src/weatherterm/core/unit.py
+++
@@ -1,8 +0,0 @@
-from enum import auto, unique
-
-from .base_enum import BaseEnum
-
-@unique
-class Unit(BaseEnum):
-    CELSIUS = auto()
-    FAHRENHEIT = auto()
