c0db521e820aefccd8660caa60fe4864552863ec
lallemann <pro.louis.allemann@gmail.com>
2020-04-29 21:43:52 +0200

    added some helper methods for weather_com_parser.py

--- src/weatherterm/parsers/weather_com_parser.py
+++ src/weatherterm/parsers/weather_com_parser.py
@@ -1,5 +1,9 @@
+import re
 
 from weatherterm.core import ForecastType
+from weatherterm.core import Request
+from weatherterm.core import Unit
+from weatherterm.core import UnitConverter
 
 class WeatherComParser:
 
@@ -10,6 +14,12 @@ class WeatherComParser:
                 ForecastType.TENDAYS: self._five_and_ten_days_forecast,
                 ForecastType.WEEKEND: self._weekend_forecast,
                 }
+        self._base_url = 'http://weather.com/weather/{forecast}/l/{area}'
+        self._request = Request(self._base_url)
+
+        self._temp_regex = re.compile('([0-9]+)\D{,2}([0-9]+)')
+        self._only_digits_regex = re.compile('[0-9]+')
+        self._unit_converter = UnitConverter(Unit.CELSIUS)
 
     def _today_forecast(self, args):
         raise NotImplementedError()
@@ -19,6 +29,19 @@ class WeatherComParser:
 
     def _weekend_forecast(self, args):
         raise NotImplementedError()
+
+    def _get_data(self, container, search_items):
+        scraped_data = {}
+
+        for key, value in search_items.items():
+            result = container.find(value, class_=key)
+
+            data = None if result is None else result.get_text()
+
+            if data is not None:
+                scraped_data[key] = data
+
+        return scraped_data
 
     def run(self, args):
         self._forecast_type = args.forecast_option
