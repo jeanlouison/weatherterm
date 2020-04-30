0ff0a6902577bba3a77bf9f81339d275809aec88
lallemann <pro.louis.allemann@gmail.com>
2020-04-30 16:36:12 +0200

    fixed phantomjs

--- src/weatherterm/parsers/weather_com_parser.py
+++ src/weatherterm/parsers/weather_com_parser.py
@@ -1,5 +1,7 @@
 import re
 
+from bs4 import BeautifulSoup
+
 from weatherterm.core import ForecastType
 from weatherterm.core import Request
 from weatherterm.core import Unit
@@ -22,7 +24,42 @@ class WeatherComParser:
         self._unit_converter = UnitConverter(Unit.CELSIUS)
 
     def _today_forecast(self, args):
-        raise NotImplementedError()
+        criteria = {
+                'today_nowcard-temp': 'div',
+                'today_nowcard-phrase': 'div',
+                'today_nowcard-hilo': 'div'
+                }
+        content = self._request.fetch_data(args.forecast_option.value, args.area_code)
+
+        bs = BeautifulSoup(content, 'html_parser')
+        container = bs.find('section', class_='today_nowcard-container')
+
+        weather_conditions = self._parse(container, criteria)
+
+        if len(weather_conditions) < 1:
+            raise Exception('Could not parse weather forecast for today.')
+
+        weatherinfo = weather_conditions[0]
+
+        temp_regex = re.compile(('H\s+(\d+|\-{,2}).+' 'L\s+(\d+|\-{,2})'))
+        temp_info = temp_regex.search(weatherinfo['today_nowcard-hilo'])
+
+        high_temp, low_temp = temp_info.groups()
+
+        side = container.find('div', class_='today_nowcard-sidecar')
+        humidity, wind = self._get_additional_info(side)
+
+        curr_temp = self._clear_str_number(weatherinfo['today_nowcard-temp'])
+
+        self._unit_converter.dest_unit = args.unit
+        td_forecast = Forecast(self._unit_converter.convert(curr_temp),
+                humidity,
+                wind,
+                high_temp=self._unit_converter.convert(high_temp),
+                low_temp=self.unit_converter.convert(low_temp),
+                description=weatherinfo['today_nowcard-phrase'])
+
+        return [td_forecast]
 
     def _five_and_ten_days_forecast(self, args):
         raise NotImplementedError()
@@ -42,6 +79,20 @@ class WeatherComParser:
                 scraped_data[key] = data
 
         return scraped_data
+
+    def _parse(self, container, criteria):
+        results = [self._get_data(item, criteria)
+                for item in container.children]
+        return [result for result in results if result]
+
+    def _clear_str_number(self, str_number):
+        result = self._only_digits_regex.match(str_number)
+        return '--' if result is None else result.group()
+
+    def _get_additional_info(self, content):
+        data = tuple(item.td.span.get_text()
+                for item in content.table.tbody.children)
+        return data[:2]
 
     def run(self, args):
         self._forecast_type = args.forecast_option
